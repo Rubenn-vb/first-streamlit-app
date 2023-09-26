d2c5930c5f8821f35d830b383828cb29657e8850
Rubenn-vb <63058852+Rubenn-vb@users.noreply.github.com>
2023-09-26 21:44:59 +0200

    Update streamlit_app.py
    Populated the fruit picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ streamlit.text("🥑🍞 Avocado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # display fruit selector
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 # display table
 streamlit.dataframe(my_fruit_list)
